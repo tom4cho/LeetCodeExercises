15f0a425b6cabf0d131915cdcd84bd1c9aa40f65
Usuario Consulta en Linea Pedregal <ClPedregal@comfama.edu.co>
2025-06-06 12:28:24 -0500

    Add Exercise_1_X.js ; git commit -m Add

---
+++ Exercise_1_X.js
@@ -0,0 +1,24 @@
+/*
+    Dada una cadena s, devolver la subcadena palindroma más larga en s.
+    (la cadena palindroma es aquella que se lee igual al revez).
+
+    Objetivo:
+        Devolver la subcadena palindroma más larga en s.
+
+    Sub objetivos:
+        1.Encontrar todas las subcadenas posibles en s.
+        2.Buscar la subcadena palindroma mas larga.
+        3.devolver la subcadena palindroma más larga.
+
+
+    Algoritmo para encontrar todas las subcadenas posibles:
+        Datos de entrada:
+            
+        Datos de salida:
+
+    Algoritmo para buscar la subcadena palindroma más larga:
+        Datos de entrada:
+        
+        Datos de salida:
+
+*/
